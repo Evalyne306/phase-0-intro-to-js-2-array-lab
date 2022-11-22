60a0f34bd129d505a0c5ecb850b8cbbf5dab4c0c
Evalyne306 <evalyne.wambui@student.moringaschool.com>
2022-11-22 13:01:59 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Write your solution here!
+const cats =["Milo", "Otis", "Garfield"];
+
+function  destructivelyAppendCat(){
+    cats.push("Ralph")
+
+}
+
+function  destructivelyPrependCat(){
+    cats.unshift("Bob")
+
+}
+
+function  destructivelyRemoveLastCat(){
+    cats.pop("Garfield")
+ 
+}
+
+function  destructivelyRemoveFirstCat(){
+    cats.shift("Milo")
+   
+}
+
+function  appendCat(){
+    const newArray =[...cats,"Broom" ]
+    return newArray
+ 
+}
+
+function  prependCat(){
+    const newArray = [...cats]
+    newArray.unshift("Arnold")
+    return newArray
+
+ }
+
+ 
+ function  removeFirstCat(){
+  var newArray = cats.slice(1)
+  return newArray;
+  }
+ function removeLastCat(){
+  var newArray =cats.slice(0,-1)
+  return newArray;
+ }
+    
+
+
+
+
+
+
